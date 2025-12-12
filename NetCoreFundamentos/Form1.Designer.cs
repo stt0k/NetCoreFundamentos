04d85f06ace42837b602a496fcb90c334f26c223
stt0k <espstok04@gmail.com>
2025-12-12 10:20:18 +0100

    First Commit

--- NetCoreFundamentos/Form1.Designer.cs
+++ NetCoreFundamentos/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnPulsar = new Button();
+            label1 = new Label();
+            txtNombre = new TextBox();
+            lblNombre = new Label();
+            SuspendLayout();
+            // 
+            // btnPulsar
+            // 
+            btnPulsar.BackColor = SystemColors.Control;
+            btnPulsar.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPulsar.ForeColor = SystemColors.ControlText;
+            btnPulsar.Location = new Point(151, 200);
+            btnPulsar.Margin = new Padding(5);
+            btnPulsar.Name = "btnPulsar";
+            btnPulsar.Size = new Size(258, 67);
+            btnPulsar.TabIndex = 0;
+            btnPulsar.Text = "Pulsar";
+            btnPulsar.UseVisualStyleBackColor = false;
+            btnPulsar.Click += btnPulsar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(151, 90);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Introduzca su nombre:";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(151, 135);
+            txtNombre.Margin = new Padding(5);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(350, 33);
+            txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(151, 298);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(0, 25);
+            lblNombre.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(601, 393);
+            Controls.Add(lblNombre);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Controls.Add(btnPulsar);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnPulsar;
+        private Label label1;
+        private TextBox txtNombre;
+        private Label lblNombre;
     }
 }
--- NetCoreFundamentos/Form1.cs
+++ NetCoreFundamentos/Form1.cs
@@ -6,5 +6,14 @@ namespace NetCoreFundamentos
         {
             InitializeComponent();
         }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            this.txtNombre.Location = new Point(10, 30);
+            this.txtNombre.Text = "Soy un texto nuevo!!";
+            this.txtNombre.Width = 220;
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            this.txtNombre.BackColor = Color.Fuchsia;
+        }
     }
 }
